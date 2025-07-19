7ecea3f213883148ff563adfeb1dea0d622bd8b0
ahmed bahgat <ahmed.bahgat@centria.fi>
2025-07-19 17:16:32 +0300

    Guess My Number

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too low!';
+  }
+});
